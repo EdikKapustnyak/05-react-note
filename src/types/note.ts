8f0bf869af09e55334325b6dcafa729ca4cabdd4
kapustnyak <zned003@gmail.com>
2025-07-24 02:13:23 +0200

    first commit

---
+++ src/types/note.ts
@@ -0,0 +1,18 @@
+export interface Note {
+  id: number;
+  title: string;
+  content: string;
+  tag: string;
+  createdAt: string;
+    updatedAt: string;
+}
+
+export interface NewNoteData {
+  title: string;
+  content: string;
+  tag: string;
+}
+
+export type NoteTag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
+
+
